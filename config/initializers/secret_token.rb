9e40a053f457f9cf7f7ef876d824a0496922c1cc
rejinreeza <rejinreeza@gmail.com>
2014-01-31 16:43:56 +0530

    game of life rails v3r193

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameOfLife::Application.config.secret_token = '656773846fc4529388e433ad41884f40e33fb34992b3c3dc5bc025ae21fbbeb560a49e3108e0858a5ec59e2032402bd7383d16511c314f73ecb75af000dfb37c'
